58394abd25d4d81cb6f7b9c381159b1c5418cdbd
qyf381389362 <381389362@qq.com>
2018-11-14 18:08:53 +0800

    完成页面初步效果

--- src/components/period/period.js
+++ src/components/period/period.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Drawer } from 'antd';
+import { Form, Drawer, Button, Input, notification } from 'antd';
 import hljs from 'highlight.js/lib/highlight';
 import xml from 'highlight.js/lib/languages/xml';
+import axios from 'axios';
 
 import PlanItems from '../period/plan/planItems';
 import PlanItemsVerify from '../period/plan/planItemsVerify';
@@ -11,9 +12,12 @@ import DesignItems from '../period/design/designItems';
 import DesignItemsVerify from '../period/design/designItemsVerify';
 import CodingItems from '../period/coding/codingItems';
 import CodingItemsVerify from '../period/coding/codingItemsVerify';
-import style from './period.less';
+import './period.css';
 
 import XML from '../../xml/xml';
+
+const FormItem = Form.Item;
+const { TextArea } = Input;
 
 class Period extends Component {
   constructor(props) {
@@ -21,17 +25,20 @@ class Period extends Component {
 
     this.state = {
       visible: false,
+      drawerTitle: '',
       drawerContent: ''
     }
 
     this.showDrawer = this.showDrawer.bind(this);
     this.onClose = this.onClose.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   showDrawer (record) {
     this.setState({
       visible: true,
-      drawerContent: record.name
+      drawerTitle: record.name,
+      isEdit: false
     });
   }
 
@@ -41,14 +48,47 @@ class Period extends Component {
     });
   }
 
+  handleSubmit (e) {
+    e.preventDefault();
+    let drawerContent = this.props.form.getFieldValue('content');
+    console.log(drawerContent);
+    if (this.state.isEdit) {
+      // 修改内容点击提交时触发
+      // alert(`提交 ${this.state.drawerTitle}`);
+      // console.log(this.state.drawerContent);
+      axios.post('/api/save', {
+        key: this.state.drawerTitle,
+        content: drawerContent
+      }).then((res) => {
+        console.log(res.data);
+        notification.success({
+          message: '修改成功'
+        });
+      });
+    }else {
+      // alert(`修改 ${this.state.drawerTitle}`);
+      // console.log(this.state.drawerContent);
+      // notification.error({
+      //   message: '修改失败'
+      // });
+      axios.get('/api/test').then((res) => {
+        console.log(res.data);
+      });
+    }
+    this.setState({
+      isEdit: !this.state.isEdit
+    });
+  }
+
   componentDidMount() {
     hljs.registerLanguage('xml', xml);
     hljs.initHighlightingOnLoad();
   }
 
   render () {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div className={ style.container }>
+      <div className="container">
         <PlanItems showDrawer={ this.showDrawer }/>
         <PlanItemsVerify showDrawer={ this.showDrawer }/>
         <RequireItems showDrawer={ this.showDrawer }/>
@@ -59,18 +99,46 @@ class Period extends Component {
         <CodingItemsVerify showDrawer={this.showDrawer }/>
 
         <Drawer
-          title={ `${this.state.drawerContent}内容` }
+          title={ `${this.state.drawerTitle}内容` }
           placement="right"
           closable={ false }
           onClose={ this.onClose }
           visible={ this.state.visible }
           width="50%"
         >
-          {/* <p>{ this.state.drawerContent }</p> */}
-          <pre><code class="xml">
-            {/* &lt;html&gt;&lt;/html&gt; */}
-            { XML[this.state.drawerContent] }
-          </code></pre>
+          <Form onSubmit={ this.handleSubmit }>
+            {
+              this.state.isEdit
+              ? (
+                  <FormItem>
+                    { getFieldDecorator('content', {})(
+                      <TextArea
+                        autosize
+                        // value={ XML[this.state.drawerTitle] }
+                        // value={ this.state.drawerContent }
+                      />
+                    ) }
+                  </FormItem>
+              )
+              : (
+                <FormItem>
+                  <pre><code className="xml">
+                    { XML[this.state.drawerTitle] }
+                  </code></pre>
+                </FormItem>
+              )
+            }
+            <FormItem>
+              <Button
+                className="mt20"
+                type="primary"
+                htmlType="submit"
+                size="large"
+              >
+                { this.state.isEdit ? '提交' : '修改' }
+              </Button>
+            </FormItem>
+          </Form>
         </Drawer>
         {/* <div>
           <pre><code class="xml">
@@ -82,4 +150,4 @@ class Period extends Component {
   }
 }
 
-export default Period;
+export default Form.create()(Period);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import App2 from './App2';
 import Total from './components/total/total';
 import Period from './components/period/period';
 
@@ -8,7 +9,7 @@ class Main extends Component {
     return (
       <div>
         <Switch>
-          <Route exact path="/" component={ Total }/>
+          <Route exact path="/" component={ App2 }/>
           <Route path="/total" component={ Total }/>
           <Route path="/period" component={ Period }/>
         </Switch>
